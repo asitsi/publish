eee0b9bed7b78947e82c152283a90800c8fa7b1e
asit singh <asitsingh0@gmail.com>
2020-09-24 02:08:30 +0530

    Updates

---
+++ precache-manifest.51d32a80f4ab5ce37b45fdb5f52907d0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176314043ff39c9c1a2980b8c9c1c843",
+    "url": "/publish/index.html"
+  },
+  {
+    "revision": "8686197ecf00f8939037",
+    "url": "/publish/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "6a07508643106a1d24f8",
+    "url": "/publish/static/css/main.eb922b3b.chunk.css"
+  },
+  {
+    "revision": "8686197ecf00f8939037",
+    "url": "/publish/static/js/2.7e066302.chunk.js"
+  },
+  {
+    "revision": "c3af2aa7bbf7561b9d2cc21ffee3ea4a",
+    "url": "/publish/static/js/2.7e066302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a07508643106a1d24f8",
+    "url": "/publish/static/js/main.07b66437.chunk.js"
+  },
+  {
+    "revision": "714e3e15ef0626fa4af6",
+    "url": "/publish/static/js/runtime-main.585e1eb8.js"
+  },
+  {
+    "revision": "6798322b95ec9fbf407b3364fec44b12",
+    "url": "/publish/static/media/20859.6798322b.jpg"
+  },
+  {
+    "revision": "56fd849e9c022339518b9f9cbb4afba5",
+    "url": "/publish/static/media/2170435.56fd849e.jpg"
+  },
+  {
+    "revision": "515afb9023cf439d1bc168fea08ec7f7",
+    "url": "/publish/static/media/631.515afb90.jpg"
+  },
+  {
+    "revision": "effcd6560b0e59b30c54f5192c47ded6",
+    "url": "/publish/static/media/632.effcd656.jpg"
+  },
+  {
+    "revision": "a75f74ee7f8a0f1241ff5257c542bbb2",
+    "url": "/publish/static/media/6547.a75f74ee.jpg"
+  },
+  {
+    "revision": "d015cc2938876d58adee564a3a042836",
+    "url": "/publish/static/media/Busines.d015cc29.jpg"
+  },
+  {
+    "revision": "a29d676cf1548fd81d2116c417a5437e",
+    "url": "/publish/static/media/accounting.a29d676c.jpg"
+  },
+  {
+    "revision": "ccb0990b1ad80317761ba1462d42e0a6",
+    "url": "/publish/static/media/art.ccb0990b.jpg"
+  },
+  {
+    "revision": "20d971e8444437278ceb100175c85a64",
+    "url": "/publish/static/media/consultancy2.20d971e8.png"
+  },
+  {
+    "revision": "ef60b665bbbb93d7f162b465fad0f3b8",
+    "url": "/publish/static/media/edu_1.ef60b665.png"
+  },
+  {
+    "revision": "972ed0fc1aa281508897b200f9d70a62",
+    "url": "/publish/static/media/english.972ed0fc.jpg"
+  },
+  {
+    "revision": "35d89a8f1c005a15f518468c24497bb4",
+    "url": "/publish/static/media/h&f.35d89a8f.jpg"
+  },
+  {
+    "revision": "383fc7767386d08b6b5042bf5f7ec89a",
+    "url": "/publish/static/media/logo.383fc776.png"
+  },
+  {
+    "revision": "aec05d4156b35af41660e3a479288bfc",
+    "url": "/publish/static/media/map.aec05d41.jpg"
+  },
+  {
+    "revision": "b26d89f637aaa6761adeb684f6b0cd9f",
+    "url": "/publish/static/media/math.b26d89f6.jpg"
+  },
+  {
+    "revision": "9e93c20a471549a421a1bc77beb2b920",
+    "url": "/publish/static/media/photography.9e93c20a.jpg"
+  },
+  {
+    "revision": "f85aa714ab3026beaf660938830edb36",
+    "url": "/publish/static/media/science.f85aa714.jpg"
+  }
+]);
