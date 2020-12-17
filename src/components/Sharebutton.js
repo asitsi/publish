31a783004e6e8e97fd9913dd38c006025bea9875
asitsi <41507591+asitsi@users.noreply.github.com>
2020-12-18 03:38:41 +0530

    Update Sharebutton.js

--- src/components/Sharebutton.js
+++ src/components/Sharebutton.js
@@ -1,1 +1,26 @@
+import React, { Component } from "react";
+import ShareButton from "react-web-share-button";
+
+const Sharebutton = () => {
+  return (
+    <>
+      <ShareButton
+        title="My Great Page"
+        text="A really great page"
+        url="http://www.divetolearn.com"
+        buttonStyle={{
+          color: "#ffca08",
+          background: "#fff",
+          width: "100%",
+          padding: "10px 12px",
+          border: "none",
+          position: "absolute",
+          left: "0",
+        }}
+      />
+    </>
+  );
+};
+
+export default Sharebutton;
 
